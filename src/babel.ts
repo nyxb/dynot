bccdea4fbf0d1c0a582ad24a5d52929436ec7b71
nyxb <contact@nyxb.xyz>
2023-05-10 07:16:15 +0200

    🆕 feat(babel.ts): add support for TypeScript and legacy syntax in Babel transform This commit adds support for TypeScript and legacy syntax in the Babel transform. The `transform` function now accepts a `ts` and `legacy` option, which when set to true, adds the necessary plugins to support TypeScript and legacy syntax. The `@babel/plugin-transform-typescript`, `babel-plugin-transform-typescript-metadata`, `@babel/plugin-proposal-decorators`, `babel-plugin-parameter-decorator`, `@babel/plugin-syntax-import-assertions`, `@babel/plugin-proposal-nullish-coalescing-operator`, and `@babel/plugin-proposal-optional-chaining` plugins are added to the list of plugins to be used in the transform.

---
+++ src/babel.ts
@@ -0,0 +1,78 @@
+import { transformSync } from '@babel/core'
+import type {
+   TransformOptions as BabelTransformOptions,
+   PluginItem,
+} from '@babel/core'
+import type { TRANSFORM_RESULT, TransformOptions } from './types'
+import { TransformImportMetaPlugin } from './plugins/babel-plugin-transform-import-meta'
+import { importMetaEnvPlugin } from './plugins/import-meta-env'
+
+export default function transform(opts: TransformOptions): TRANSFORM_RESULT {
+   const _opts: BabelTransformOptions & { plugins: PluginItem[] } = {
+      babelrc: false,
+      configFile: false,
+      compact: false,
+      retainLines:
+      typeof opts.retainLines === 'boolean' ? opts.retainLines : true,
+      filename: '',
+      cwd: '/',
+      ...opts.babel,
+      plugins: [
+         [
+            require('@babel/plugin-transform-modules-commonjs'),
+            { allowTopLevelThis: true },
+         ],
+         [require('babel-plugin-dynamic-import-node'), { noInterop: true }],
+         [TransformImportMetaPlugin, { filename: opts.filename }],
+         [require('@babel/plugin-syntax-class-properties')],
+         [require('@babel/plugin-proposal-export-namespace-from')],
+         [importMetaEnvPlugin],
+      ],
+   }
+
+   if (opts.ts) {
+      _opts.plugins.push([
+         require('@babel/plugin-transform-typescript'),
+         { allowDeclareFields: true },
+      ])
+      // `unshift` because these plugin must come before `@babel/plugin-syntax-class-properties`
+      _opts.plugins.unshift(
+         [require('babel-plugin-transform-typescript-metadata')],
+         [require('@babel/plugin-proposal-decorators'), { legacy: true }],
+      )
+      _opts.plugins.push(require('babel-plugin-parameter-decorator'))
+      _opts.plugins.push(require('@babel/plugin-syntax-import-assertions'))
+   }
+
+   if (opts.legacy) {
+      _opts.plugins.push(
+         require('@babel/plugin-proposal-nullish-coalescing-operator'),
+      )
+      _opts.plugins.push(require('@babel/plugin-proposal-optional-chaining'))
+   }
+
+   if (opts.babel && Array.isArray(opts.babel.plugins))
+      _opts.plugins?.push(...opts.babel.plugins)
+
+   try {
+      return {
+         code: transformSync(opts.source, _opts)?.code || '',
+      }
+   }
+   catch (error: any) {
+      return {
+         error,
+         code:
+        `exports.__DYNOT_ERROR__ = ${
+        JSON.stringify({
+           filename: opts.filename,
+           line: error.loc?.line || 0,
+           column: error.loc?.column || 0,
+           code: error.code
+              ?.replace('BABEL_', '')
+              .replace('PARSE_ERROR', 'ParseError'),
+           message: error.message?.replace('/: ', '').replace(/\(.+\)\s*$/, ''),
+        })}`,
+      }
+   }
+}
